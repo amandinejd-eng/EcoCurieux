58b89a1084db366c50a4c25ddd47e3f417e25389
geoffray <geoffraygaetan@gmail.com>
2026-03-17 18:56:38 +0100

    Supp

--- cleanup.ps1
+++
@@ -1,3 +0,0 @@
-$content = Get-Content "c:\Users\geoff\Desktop\EcoCurieux\modele1_v2 (16).html"
-$cleanContent = $content | Where-Object { $_ -notmatch "/2wBDAAkGBwgHBgkIBwgKCgkLDRYPDQwMDRsUFRAWIB0iIiAdHx8kKDQsJCYxJx8fLT0tMTU3Ojo6Iys/RD84QzQ5Ojf/2wBDAQoKCg0MDRoPDxo3JR8lNzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzf" }
-$cleanContent | Set-Content "c:\Users\geoff\Desktop\EcoCurieux\modele1_v2 (16).html"
--- cleanup2.ps1
+++
@@ -1,9 +0,0 @@
-$content = Get-Content "c:\Users\geoff\Desktop\EcoCurieux\modele1_v2 (16).html"
-$cleanContent = $content | ForEach-Object {
-    if ($_ -match "/2wBDAAkGBwgHBgkIBwgKCgkLDRYPDQwMDRsUFRAWIB0iIiAdHx8kKDQsJCYxJx8fLT0tMTU3Ojo6Iys/RD84QzQ5Ojf/2wBDAQoKCg0MDRoPDxo3JR8lNzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzf") {
-        $_ -replace "/2wBDAAkGBwgHBgkIBwgKCgkLDRYPDQwMDRsUFRAWIB0iIiAdHx8kKDQsJCYxJx8fLT0tMTU3Ojo6Iys/RD84QzQ5Ojf/2wBDAQoKCg0MDRoPDxo3JR8lNzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzf.*$", ""
-    } else {
-        $_
-    }
-}
-$cleanContent | Set-Content "c:\Users\geoff\Desktop\EcoCurieux\modele1_v2 (16).html"
